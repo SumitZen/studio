f33d3d1654fec782c84cabbf7a949563d1d336c6
Sumit Bhalse <ss1816434@gmail.com>
2025-09-21 16:32:34 +0000

    the ui is not responisve

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -6,7 +6,7 @@ export default function DashboardLayout({ children }: { children: ReactNode }) {
   return (
     <div className="flex min-h-screen w-full flex-col bg-muted/40">
       <DashboardSidebar />
-      <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
+      <div className="flex flex-col sm:gap-4 sm:py-4 md:pl-14">
         <DashboardHeader />
         <main className="flex-1 gap-4 p-4 sm:px-6 sm:py-0 md:gap-8">
           {children}
